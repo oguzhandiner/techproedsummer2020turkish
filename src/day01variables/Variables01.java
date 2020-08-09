fca618db85a6c01886ce554663c1a324f43dda2f
Oguzhan <oguzhandiner061@gmail.com>
2020-08-09 23:38:05 +0300

    cakisma sorunu cozuldu

--- src/day01variables/Variables01.java
+++ src/day01variables/Variables01.java
@@ -1,14 +1,14 @@
 package day01variables;
 
 public class Variables01 {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Welcome to Java");
 		System.out.println("ulaa ben heryerdeyim :)");
 		System.out.println("hayirdir kardes atara atar :D ");
 		System.out.println("cakisma ula");
-		
+
 	}
 
 }
